ca7c87a4aa0fe49f37d76229510759cb1f0e3b18
Marie Milena Lai Poon <71841064+Milena1995@users.noreply.github.com>
2022-11-16 22:17:07 +0400

    update features components

--- src/components/services/ReverseFeatures.tsx
+++ src/components/services/ReverseFeatures.tsx
@@ -1,43 +1,46 @@
 import { View } from "app-studio";
+import { useNavigate } from "react-router-dom";
+
 import { Horizontal, Vertical } from "src/layout/layout";
-import { H3Medium, H2lBold, H3Bold } from "../Text";
+
 import { ReactComponent as RightArrow } from "src/assets/svg/RightArrow.svg";
 
-import style from "src/stylesheet/features.module.scss";
 import { Card } from "./Card";
+import { H3Medium, H2lBold, H3Bold } from "../Text";
+
+import style from "src/stylesheet/features.module.scss";
 
 export const ReserveFeatures = () => {
+  const navigate = useNavigate();
   return (
     <section id={style.home}>
       <Horizontal id={style.content}>
         <Vertical className={style.information}>
           <H3Medium color="#5BCCDE">Service</H3Medium>
-          <H2lBold marginVertical={20} color="#486A6F">
-            Featured Listing
-          </H2lBold>
+          <H2lBold id={style.subTitle}>Featured Listing</H2lBold>
           <H3Medium color="#678C92">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit
             sem, vestibulum sed leo eu, malesuada pharetra mauris. Integer
             rhoncus, eros vel euismod tempor, ipsum magna tristique nisi, quis
             ullamcorper enim magna eu orci.
           </H3Medium>
-          <Horizontal id={style.features_card} marginTop={100}>
+          <Horizontal id={style.features_card}>
             <Card />
             <Card />
           </Horizontal>
-          <Horizontal
-            gap={20}
-            color={"#486A6F"}
-            alignSelf={"flex-end"}
-            marginTop={"auto"}
-            justifyContent={"center"}
-            alignItems="center"
-          >
-            <H3Bold>See All</H3Bold>
-            <RightArrow />
+          <Horizontal id={style.see_all}>
+            <H3Bold
+              id={style.see_all_text}
+              onClick={() => navigate("/services")}
+            >
+              See All
+            </H3Bold>
+            <RightArrow
+              id={style.see_all_arrow}
+              onClick={() => navigate("/services")}
+            />
           </Horizontal>
         </Vertical>
-
         <View className={style.img_container}>
           <img
             id={style.img}
