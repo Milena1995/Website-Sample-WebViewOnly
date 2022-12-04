f99aa5c756c1fc046fb7b5e10bd7f9ec9ccacd03
Marie Milena Lai Poon <71841064+Milena1995@users.noreply.github.com>
2022-12-04 20:56:46 +0400

    update contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,19 @@
-import { View } from "app-studio";
+import { ContactContent } from "src/components/contact/ContactContent";
+import { ContactTile } from "src/components/contact/ContactTile";
+import { NavBar } from "src/components/navigation/NavBar";
+import { PageLayout } from "src/layout/pageLayout";
 
 import style from "src/stylesheet/contact/contact.module.scss";
 
 const ContactPage = () => {
-  return <View id={style.container}>Hello</View>;
+  return (
+    <PageLayout>
+      <section id={style.contactContent}>
+        <NavBar />
+        <ContactContent />
+      </section>
+    </PageLayout>
+  );
 };
 
 export default ContactPage;
